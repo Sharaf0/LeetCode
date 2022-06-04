11ee4f8613e9734ec78d6ed8a608522372b36361
Mohammad Sharaf <mohammedsharaf.1992@gmail.com>
2022-06-04 18:12:13 +0200

    Update 78. Subsets.cpp

--- 78. Subsets.cpp
+++ 78. Subsets.cpp
@@ -29,3 +29,22 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+        vector<int> temp;
+        for(int i = 0; i < (1<<n); i ++) {
+            for(int j = 0; j < n; j ++) {
+                if(((1 << j) & i)) {
+                    temp.push_back(nums[j]);
+                }
+            }
+            ans.push_back(temp);
+            temp.clear();
+        }
+        return ans;
+    }
+};
